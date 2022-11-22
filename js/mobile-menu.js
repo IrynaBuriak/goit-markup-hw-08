df4cb5a81ad5991bb1e018a8e40328e3ed1407bc
IrynaBuriak <irina.buriak13@gmail.com>
2022-11-22 14:17:46 +0200

    бургер

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,32 +1,3 @@
-// (() => {
-//   const mobileMenu = document.querySelector('.js-menu-container');
-//   const openMenuBtn = document.querySelector('.js-open-menu');
-//   const closeMenuBtn = document.querySelector('.js-close-menu');
-
-//   const toggleMenu = () => {
-//     const isMenuOpen =
-//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-//     mobileMenu.classList.toggle('is-open');
-
-//     const scrollLockMethod = !isMenuOpen
-//       ? 'disableBodyScroll'
-//       : 'enableBodyScroll';
-//     bodyScrollLock[scrollLockMethod](document.body);
-//   };
-
-//   openMenuBtn.addEventListener('click', toggleMenu);
-//   closeMenuBtn.addEventListener('click', toggleMenu);
-
-//   // Close the mobile menu on wider screens if the device orientation changes
-//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-//     if (!e.matches) return;
-//     mobileMenu.classList.remove('is-open');
-//     openMenuBtn.setAttribute('aria-expanded', false);
-//     bodyScrollLock.enableBodyScroll(document.body);
-//   });
-// })();
-
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
@@ -44,7 +15,7 @@
     });
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
         if (!e.matches) return;
-        menuBtnRef.classList.remove('is-open');
+        menuBtnRef.classList.add('is-open');
         mobileMenuRef.classList.remove('is-open');
         wrapper.classList.remove("lock");
         menuBtnRef.setAttribute('aria-expanded', false);
